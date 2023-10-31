c6da363bfc62fb204002f9dd2d144ab2f6d7b6aa
aakash-301 <aakashshivanshu5@gmail.com>
2023-10-31 15:49:33 +0530

    added api files for the applicaton

---
+++ src/lib/appwrite/api.ts
@@ -0,0 +1,83 @@
+import { INewUser } from "@/types";
+import { account, appwriteConfig, avatars, databases } from "./config";
+import { log } from "console";
+import { ID, Query } from "appwrite";
+
+export async function createUserAccount(user: INewUser) {
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      user.email,
+      user.password,
+      user.name
+    );
+
+    if (!newAccount) throw Error;
+
+    const avatarUrl = avatars.getInitials(user.name);
+
+    const newUser = await saveUserToDb({
+      accountId: newAccount.$id,
+      email: newAccount.email,
+      name: newAccount.name,
+      username: user.username,
+      imageUrl: avatarUrl,
+    });
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+
+export async function saveUserToDb(user: {
+  accountId: string;
+  email: string;
+  name: string;
+  imageUrl: URL;
+  username?: string;
+}) {
+  try {
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      user
+    );
+
+    return newUser;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function signInAccount(user: { email: string; password: string }) {
+  try {
+    const session = await account.createEmailSession(user.email, user.password);
+
+    return session;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function getCurrentUser() {
+  try {
+    const currentAcount = await account.get();
+
+    if (!currentAcount) throw Error;
+
+    const currentUser = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      [Query.equal("accountId", currentAcount.$id)]
+    );
+
+    if (!currentUser) throw Error;
+
+    return currentUser.documents[0];
+  } catch (err) {
+    console.log(err);
+  }
+}
