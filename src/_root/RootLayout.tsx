d5aa744f217e651b27da9ce911c3f1f96f69ac70
aakash-301 <aakashshivanshu5@gmail.com>
2023-11-07 19:44:47 +0530

    added contents for rootlayout

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import Bottombar from '@/components/shared/Bottombar'
+import Topbar from '@/components/shared/Topbar'
+import { Outlet } from 'react-router-dom'
 
 function RootLayout() {
   return (
-    <div>RootLayout</div>
+    <div className='w-full md:flex'>
+      <Topbar />
+      <LeftSidebar />
+      <section className='flex fex-1 h-full'>
+        <Outlet />
+      </section>
+      <Bottombar />
+    </div>
   )
 }
 
