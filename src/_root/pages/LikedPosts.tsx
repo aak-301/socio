8ca9f8e7ac8c889622c063e22b88c98310d96c30
aakash-301 <aakashshivanshu5@gmail.com>
2023-11-08 20:27:59 +0530

    feat:Developd each screen for each tab

--- src/_root/pages/LikedPosts.tsx
+++ src/_root/pages/LikedPosts.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
 
-function LikedPosts() {
+import GridPostList from "@/components/shared/GridPostList";
+import Loader from "@/components/shared/Loader";
+import { useGetCurrentUser } from "@/lib/react-query/queries";
+
+const LikedPosts = () => {
+  const { data: currentUser } = useGetCurrentUser();
+
+  if (!currentUser)
+    return (
+      <div className="flex-center w-full h-full">
+        <Loader />
+      </div>
+    );
+
   return (
-    <div>LikedPosts</div>
-  )
-}
+    <>
+      {currentUser.liked.length === 0 && (
+        <p className="text-light-4">No liked posts</p>
+      )}
 
-export default LikedPosts
+      <GridPostList posts={currentUser.liked} showStats={false} />
+    </>
+  );
+};
+
+export default LikedPosts;
